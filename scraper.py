08e2e453865d881da6c1be2c0ea5ed52bdefb9c7
ff077 <miftahf77@gmail.com>
2022-06-25 21:25:11 +0700

    add more metadata

--- scraper.py
+++ scraper.py
@@ -8,6 +8,8 @@ BASEURL = "https://www.thewhiskyexchange.com"
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'
     }
+FILEDIR = "./"
+FILENAME = "whisky_products"
 
 k = requests.get('https://www.thewhiskyexchange.com/c/35/japanese-whisky').text
 soup = BeautifulSoup(k,'html.parser')
@@ -49,32 +51,60 @@ for link in productlinks:
     except:
         about = None
 
+    # try:
+    #     rating = hun.find("div",{"class":"review-overview"}).text.replace('\n',"")
+    # except:
+    #     rating = None
+
     try:
-        rating = hun.find("div",{"class":"review-overview"}).text.replace('\n',"")
+        rating = hun.find("span", class_="review-overview__rating").text.strip()
     except:
         rating = None
+
+    try:
+        review_count = hun.find("span", class_="review-overview__count").text
+        review_count = review_count.encode('ascii', errors='ignore').decode("utf-8").split("Re", 1)[0].replace("(","").strip()
+    except:
+        review_count = None
 
     try:
         name = hun.find("h1",{"class":"product-main__name"}).text.replace('\n',"")
     except:
         name = None
 
-    whisky = {"name":name,"price":price,"rating":rating,"about":about}
+    try:
+        made_style = hun.find("ul", class_="product-main__meta")
+        made_style = list(made_style.descendants)[1].text.strip()
+    except:
+        made_style = None
 
+    try:
+        img_url = hun.find("img", class_="product-main__image")["src"]
+    except:
+        img_url = None
+
+    whisky = {
+        "name":name,
+        "made style":made_style,
+        "price":price,
+        "rating":rating,
+        "Number of reviews":review_count,
+        "about":about,
+        "image_url":img_url,
+        }
     data.append(whisky)
+    # print(data)
     # break
 
 print("Get product details completed.")
 print("Scraping is finished in %s seconds." % (time.time()-start_time))
 
 df = pd.DataFrame(data)
-# print(df.head())
+print(df.head())
 
-FILEDIR = "./"
-FILENAME = "whisky_products.xlsx"
-
-df.to_excel(FILEDIR + FILENAME)
-print("Data is saved to %s." % (FILEDIR + FILENAME))
+df.to_excel("%s.xlsx" % (FILEDIR + FILENAME))
+df.to_csv("%s.csv" % (FILEDIR + FILENAME), index=False)
+print("Data is already saved to xlsx and csv format.")
 
 
 
